3c52c9e5271c7f33bcee20d016ea0b60a21381db
Victor <victorhmatines@gmail.com>
2023-04-17 21:22:28 -0300

    17-04-2023

--- src/test/java/com/ada/aulajunit/CalculadoraParameterizedTest.java
+++ src/test/java/com/ada/aulajunit/CalculadoraParameterizedTest.java
@@ -1,21 +1,20 @@
 package com.ada.aulajunit;
 
 import com.ada.aulajunit.modelo.Calculadora;
-import net.joshka.junit.json.params.JsonFileSource;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculadoraParameterizedTest {
 
     @ParameterizedTest(name="Caso {index}: Passando {0} e {1} deveria retornar {2}")
-//    @CsvFileSource(resources="/dados.csv")
+    @CsvFileSource(resources="/dados.csv")
 
-    @JsonFileSource(resources="/dados.json")
+//    @JsonFileSource(resources="/dados.json")
     public void testeSoma(int primeiro, int segundo, int expected){
 
         Calculadora calc = new Calculadora();
